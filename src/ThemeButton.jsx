40b8be2397b6eb3ede8d381e5e74c41ddb181747
Angusb111 <bodleangus@gmail.com>
2025-08-15 04:08:18 +1200

    fixed http links in svg

--- src/ThemeButton.jsx
+++ src/ThemeButton.jsx
@@ -2,7 +2,7 @@ function ThemeButton() {
     
   return (
     <button className="social-button p-2" id="darkModeToggle" onClick="toggleTheme()">
-        <svg  xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-brightness-down">
+        <svg  width="32"  height="32"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-brightness-down">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path className="sun-center" d="M12 8a4 4 0 1 1 -3.995 4.2l-.005 -.2l.005 -.2a4 4 0 0 1 3.995 -3.8z" />
             <path className="sun-ray" d="M12 4a1 1 0 0 1 .993 .883l.007 .127a1 1 0 0 1 -1.993 .117l-.007 -.127a1 1 0 0 1 1 -1z" />
